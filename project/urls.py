cd662fc89c3e38125d5af641bd0e2f34667b4059
Miozzzz <helenzyf@bu.edu>
2020-04-16 23:19:52 -0400

    new

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, ClothingPageView, RandomClothingPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', RandomClothingPageView.as_view(), name='random'),
+    path('all', HomePageView.as_view(), name='home'),
+    path('clothing/<int:pk>', ClothingPageView.as_view(), name='clothing'),
 ]
--- project/views.py
+++ project/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Clothing
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+import random
 
 class HomePageView(ListView):
     model = Clothing
     template_name = 'clothing/home.html'
     context_object_name = 'all_clothing_list'
+
+class ClothingPageView(DetailView):
+    model = Clothing
+    template_name = 'clothing/clothing.html'
+    context_object_name = 'clothing'
+
+
+class RandomClothingPageView(DetailView):
+    model = Clothing
+    template_name = 'clothing/clothing.html'
+    context_object_name = 'clothing'
+
+    def get_object(self):
+        all_clothing = Clothing.objects.all()
+        r = random.randint(0, len(all_clothing)-1 )
+        q = all_clothing[r]
+        return q
